16612736e867f74b338a93dd3812a7e9ebd5d937
Adam Arthur Faizal <adam.faizal.af6@student.uns.ac.id>
2021-03-30 16:14:46 +0700

    Add alpine js

--- System/Arthur/Core/Helper/Flasher.php
+++ System/Arthur/Core/Helper/Flasher.php
@@ -29,7 +29,7 @@ class Flasher {
         if (isset($_SESSION['flash'])):
             if ($_SESSION['flash']['type'] === 'warning'):
                 echo '
-                <div class="mb-3 flex justify-between items-center bg-yellow-200 relative text-yellow-600 py-3 px-3 rounded-lg " role="alert">
+                <div x-data="{ show: true }" x-show="show" class="mb-3 flex justify-between items-center bg-yellow-200 relative text-yellow-600 py-3 px-3 rounded-lg " role="alert">
                     <span>'. $_SESSION['flash']['message'] .' <strong>' . $_SESSION['flash']['info'] . '</strong> ' . $_SESSION['flash']['action'] . '</span>
                     <button type="button" class="close" data-dismiss="alert" aria-label="Close">
                         <span aria-hidden="true">&times;</span>
@@ -38,16 +38,16 @@ class Flasher {
                 ';
             elseif ($_SESSION['flash']['type'] === 'danger'):
                 echo '
-                <div class="mb-3 flex justify-between items-center bg-red-200 relative text-red-600 py-3 px-3 rounded-lg " role="alert">
+                <div x-data="{ show: true }" x-show="show" class="mb-3 flex justify-between items-center bg-red-200 relative text-red-600 py-3 px-3 rounded-lg " role="alert">
                     <span>'. $_SESSION['flash']['message'] .' <strong>' . $_SESSION['flash']['info'] . '</strong> ' . $_SESSION['flash']['action'] . '</span>
-                    <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+                    <button type="button" @click="show = false" class="close" data-dismiss="alert" aria-label="Close">
                         <span aria-hidden="true">&times;</span>
                     </button>
                 </div>            
                 ';
             elseif ($_SESSION['flash']['type'] === 'success'):
                 echo '
-                <div class="mb-3 flex justify-between items-center bg-green-200 relative text-green-600 py-3 px-3 rounded-lg " role="alert">
+                <div x-data="{ show: true }" x-show="show" class="mb-3 flex justify-between items-center bg-green-200 relative text-green-600 py-3 px-3 rounded-lg " role="alert">
                     <span>'. $_SESSION['flash']['message'] .' <strong>' . $_SESSION['flash']['info'] . '</strong> ' . $_SESSION['flash']['action'] . '</span>
                     <button type="button" class="close" data-dismiss="alert" aria-label="Close">
                         <span aria-hidden="true">&times;</span>
